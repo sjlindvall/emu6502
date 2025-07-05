9b1fe4561523d1e9b9a257f1b1f47d797d7b63e2
sjlindvall <sjlindvall@live.com>
2025-07-05 22:50:27 +0200

    refactored comp and hex dump

--- emu6502.pas
+++ emu6502.pas
@@ -1,7 +1,7 @@
-program EMU6502;
+program emu6502;
 
 uses
-  SysUtils, EMU6502Core;
+  SysUtils, emu6502comp;
 
 var
   Mem: TMem;
@@ -16,16 +16,31 @@ var
 
 begin
   Hellorld;
+  WriteLn;
   Mem.Init;
   Mem.WriteByte($FFFC, $10);
   Mem.WriteByte($FFFD, $00);
   Mem.WriteByte($1000, OPC_LDA_IM);
   Mem.WriteByte($1001, $42);
   Mem.WriteByte($1002, OPC_NOP);
+  Mem.WriteByte($1003, $41);
+  Mem.WriteByte($1004, $43);
+  Mem.WriteByte($1005, $44);
+  Mem.WriteByte($1006, $45);
+  Mem.WriteByte($1007, $46);
+  Mem.WriteByte($1008, $47);
+  Mem.WriteByte($1009, $48);
+  Mem.WriteByte($100A, $49);
+  Mem.WriteByte($100B, $4A);
+  Mem.WriteByte($100C, $4B);
+  Mem.WriteByte($100D, $4C);
+  Mem.WriteByte($100E, $4D);
+  Mem.WriteByte($100F, $4E);
   Mem.Show($1000, $20);
 
+  WriteLn;
   CPU.Reset(Mem);
   CPU.Execute(Mem);
-  CPU.ShowBoxedRegs;
+  CPU.ShowBoxedRegs(True);
   CPU.ShowBoxedFlags;
 end.
